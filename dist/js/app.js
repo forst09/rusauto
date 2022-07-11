74c353ebc878e1cec523f54541f456a3d6694911
Daria <dforst09@gmail.com>
2022-07-11 13:02:45 +0300

    add icons and companies

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,29 @@
 'use strict';
 
 $(document).ready(function () {
+    // ПОДКЛЮЧЕНИЕ СВАЙПЕРА В СЕКЦИИ НА ЭКРАНАХ >= 1024
+    const pageWidth = document.documentElement.scrollWidth;
+    if (pageWidth >= 1024) {
+        //ГЛАВНАЯ СТРАНИЦА СЕКЦИЯ "ТРАНСПОРТНЫЕ КОМПАНИИ, С КОТОРЫМИ МЫ РАБОТАЕМ"
+        const swiperDemanded = new Swiper('.swiper-companies', {
+            speed: 700,
+            slidesPerView: 'auto',
+            mousewheel: {
+                invert: true,
+            },
+            breakpoints: {
 
+                1024: {
+                    spaceBetween: 72,
+                },
+
+                1500: {
+                    spaceBetween: 120,
+                },
+            }
+        });
+    }
 });
+
 
 
