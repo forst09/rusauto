66c052b128d5aee46513137d7f47d7230d64dca4
Daria <dforst09@gmail.com>
2022-07-11 16:27:39 +0300

    add partners

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,26 @@ $(document).ready(function () {
     const pageWidth = document.documentElement.scrollWidth;
     if (pageWidth >= 1024) {
         //ГЛАВНАЯ СТРАНИЦА СЕКЦИЯ "ТРАНСПОРТНЫЕ КОМПАНИИ, С КОТОРЫМИ МЫ РАБОТАЕМ"
-        const swiperDemanded = new Swiper('.swiper-companies', {
+        const swiperCompanies = new Swiper('.swiper-companies', {
+            speed: 700,
+            slidesPerView: 'auto',
+            mousewheel: {
+                invert: true,
+            },
+            breakpoints: {
+
+                1024: {
+                    spaceBetween: 72,
+                },
+
+                1500: {
+                    spaceBetween: 120,
+                },
+            }
+        });
+
+        //ГЛАВНАЯ СТРАНИЦА СЕКЦИЯ "НАШИ ПАРТНЕРЫ"
+        const swiperPartners = new Swiper('.swiper-partners', {
             speed: 700,
             slidesPerView: 'auto',
             mousewheel: {
