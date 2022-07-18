2aea1f533694d680a4b3d8b8d8690213d0f82ede
Daria <dforst09@gmail.com>
2022-07-18 18:18:23 +0300

    add header-fixed

--- src/js/app.js
+++ src/js/app.js
@@ -7,18 +7,50 @@ $(document).ready(function () {
         $('.header-bottom__search-result').addClass('active');
     });
 
-    // $(document).on('click', '.header__input-reset-label', function () {
-    //     $('.header__input-search').removeClass('active');
-    //     $('.header .work').removeClass('active');
-    //     $('.header .search__result').removeClass('active');
-    //     $('.header__form-search-wrapper').removeClass('active');
-    // });
-
     //ЗАКРЫТИЕ ПОИСКА НА 1024 В ШАПКЕ
     $(document).on('blur', '.header-bottom__search-input', function () {
         $('.header-bottom__search-result').removeClass('active');
         $(this).val('');
     });
+
+    // ОТКЛЮЧЕНИЕ ZOOM ЧЕРЕЗ СКРОЛЛ (В ТОМ ЧИСЛЕ ТРЕКАПАДАМИ В MACOS)
+    document.addEventListener('mousewheel', function (e) {
+        if (!e.ctrlKey && !e.metaKey) return;
+
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    }, { passive: false });
+
+    // ОТКЛЮЧЕНИЕ ZOOM ПРИКОСНОВЕНИЯМИ (В ТОМ ЧИСЛЕ ТРЕКАПАДАМИ И Т.П.) В SAFARI И IOS
+    document.addEventListener('gesturestart', function (e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    }, { passive: false });
+
+    // ОТКЛЮЧЕНИЕ ZOOM ЧЕРЕЗ КЛАВИАТУРУ (CTRL + "+", CTRL + "-")
+    // КНОПКИ БРАУЗЕРА ДЛЯ УПРАВЛЕНИЯ ZOOM ОТКЛЮЧЕНЫ НЕ БУДУТ
+    document.addEventListener('keydown', function (e) {
+        if (!e.ctrlKey && !e.metaKey) return;
+        if (e.keyCode != 189 && e.keyCode != 187) return;
+
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    }, { passive: false });
+
+    //ФИКСИРОВАННАЯ ШАПКА НА СКРОЛЛЕ 
+    let header = document.querySelector('.header');
+    let headerFix = document.querySelector('.header-fixed');
+    let headerHeight = header.clientHeight + 200;
+    document.onscroll = function () {
+        let scroll = window.scrollY;
+
+        if (scroll > headerHeight) {
+            headerFix.classList.add('active');
+        }
+        else {
+            headerFix.classList.remove('active');
+        }
+    };
 
     //ПОДКЛЮЧЕНИЕ СВАЙПЕРА ГЛАВНАЯ СТРАНИЦА ГЛАВНЫЙ ЭКРАН
     const swiperMain = new Swiper('.swiper-main', {
@@ -32,6 +64,7 @@ $(document).ready(function () {
         },
     });
 
+    //ВОЗМОЖНОСТЬ КЛИКАТЬ ПО КНОПКЕ ГЛАВНОЙ СТРАНИЦЫ ГЛАВНОГО ЭКРАНА
     $('.btn-main').on('mousedown touchstart pointerdown', function (e) {
         e.stopPropagation();
     });
