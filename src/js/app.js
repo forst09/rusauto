a52e4f69eb3725ca4318c81f10a2b708e028ba32
forst09 <dforst09@gmail.com>
2022-08-03 17:33:45 +0300

    fin encyclopedia

--- src/js/app.js
+++ src/js/app.js
@@ -109,14 +109,14 @@ $(document).ready(function () {
 
     //ПОИСК НА 1024 В ШАПКЕ
     $(document).on('input', '.header-bottom__search-input', function () {
-        $(this).parents('.header-bottom__search').find('.header-bottom__search-result').addClass('active');
-        $(this).parents('.header-bottom__search').find('.header-bottom__search-close').addClass('active');
+        $(this).parents('.header-bottom__search-wrapper').find('.header-bottom__search-result').addClass('active');
+        $(this).parents('.header-bottom__search-wrapper').find('.header-bottom__search-close').addClass('active');
     });
 
     //ЗАКРЫТИЕ ПОИСКА НА 1024 В ШАПКЕ
     $(document).on('blur', '.header-bottom__search-input', function () {
-        $(this).parents('.header-bottom__search').find('.header-bottom__search-result').removeClass('active');
-        $(this).parents('.header-bottom__search').find('.header-bottom__search-close').removeClass('active');
+        $(this).parents('.header-bottom__search-wrapper').find('.header-bottom__search-result').removeClass('active');
+        $(this).parents('.header-bottom__search-wrapper').find('.header-bottom__search-close').removeClass('active');
         $(this).val('');
     });
 
